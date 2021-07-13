463759d7c4d9dcc4e9b37dcde4210b694319880d
linyyyang <linyyyang@tencent.com>
2021-07-13 17:16:43 +0800

    ✨ feat():first commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # koatty_views
 Template rendering middleware for Koatty.
+
+# 安装
+-----
+
+```
+npm i koatty_views
+```
+
+# 使用
+-----
+
+## Koatty
+
+1、项目中增加middleware
+
+```sh
+koatty middleware views;
+```
+
+2、修改 middleware/ViewsMiddleware.ts:
+
+```js
+import { Middleware, IMiddleware } from 'koatty';
+import { Views } from 'koatty_view';
+import { App } from '../App';
+
+@Middleware()
+export class ViewMiddleware implements IMiddleware {
+  run(options: any, app: App) {
+    return Views(options, app);
+  }
+}
+```
+
+3、项目middleware配置 config/middleware.ts:
+
+```js
+...
+// 
+ViewMiddleware: {
+    root: `${process.env.ROOT_PATH}/view`, // 模板目录
+    opts: {
+        autoRender: false,
+        extension: 'html',
+        map: {
+            html: "ejs",
+        },
+        options: {
+            cache: false, // 开发模式下修改为false, 生产环境请修改为true
+        }
+    }
+},
+...
+
+```
+
+4、 模板赋值:
+
+```js
+...
+// IndexController
+/**
+ * index 接口
+ *
+ * @returns
+ * @memberof DefaultController
+ */
+@GetMapping('')
+index(): Promise<any> {
+    this.ctx.state = { title: 'my title', author: 'queckezz' };
+    return this.ctx.render('index.html');
+}
+...
+
+```
---
+++ src/index.ts
@@ -0,0 +1,70 @@
+/*
+ * @Description:
+ * @Usage:
+ * @Author: xxx
+ * @Date: 2020-12-24 10:32:06
+ * @LastEditTime: 2021-07-13 17:12:29
+ */
+import kosViews from "koa-views";
+import { Koatty } from "koatty";
+
+
+/**
+ *
+ *
+ * @interface OptionsInterface
+ */
+export interface OptionsInterface {
+    root: string;
+    opts?: {
+        /*
+        * Whether to use ctx.body to receive the rendered template string. Defaults to true.
+        */
+        autoRender?: boolean,
+        /*
+        * Default extension for your views
+        */
+        extension?: string,
+        /*
+        * Map a file extension to an engine
+        */
+        map?: any,
+        /*
+        * replace consolidate as default engine source
+        */
+        engineSource?: any,
+        /*
+        * These options will get passed to the view engine. This is the time to add partials and helpers etc.
+        */
+        options?: any,
+    }
+}
+
+/**
+ * default options
+ */
+const defaultOptions: OptionsInterface = {
+    root: `${process.env.ROOT_PATH}/view`, // 模板目录
+    opts: {
+        autoRender: false,
+        extension: 'html',
+        map: {
+            html: "ejs",
+        },
+        options: {
+            cache: false,
+        }
+    }
+};
+/**
+ *
+ *
+ * @export
+ * @param {OptionsInterface} options
+ * @param {Application} app
+ * @returns {*}  {Koa.Middleware}
+ */
+export async function Views(options: OptionsInterface, app: Koatty) {
+    const opt = { ...defaultOptions, ...options };
+    return kosViews(opt.root, opt.opts);
+}
